c2a350733ffd426db25d0202bff6502456b2b1cd
Héctor Rábago <Hector@hectors-macbook-pro.local>
2016-03-04 16:40:57 -0500

    Initial Commit

---
+++ Clubs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clubs
+//
+//  Created by Héctor Rábago on 3/4/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
