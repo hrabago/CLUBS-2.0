1ef3b3c16e06173937061a14db002144bbc4943c
Héctor Rábago <Hector@Hectors-MacBook-Pro.local>
2016-04-26 19:54:59 -0400

    Sprint 88

--- Clubs/User.swift
+++ Clubs/User.swift
@@ -12,19 +12,41 @@ import ParseUI
 
 class User: NSObject {
     
-    var name: NSString?
-    var userEmail: NSString?
-    var screenname: NSString?
-    var profileImageUrl: NSString?
-    var dictionary: NSDictionary?
-    var imageURL: NSURL?
-    var userClubs:[NSString]?
     
     
+    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
+        // Create Parse object PFObject
+        let user = PFObject(className: "User")
+        
+        print("Saving user image")
+        
+        // Add relevant fields to the object
+        user["name"] = PFUser.currentUser() // Pointer column type that points to PFUser
+        user["profileImage"] = getPFFileFromImage(image)
+        user["events"] = 0
+        user["subscriptions"] = 0
+        
+        
+        
+        // Save object (following function will save the object in Parse asynchronously)
+        user.saveInBackgroundWithBlock(completion)
+        print("user image save complete")
+    }
     
-       
+    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
+        
+        // check if image is not nil
+        if let image = image {
+            
+            // get image data and check if that is not nil
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
     
-
+    
     
     
 }
